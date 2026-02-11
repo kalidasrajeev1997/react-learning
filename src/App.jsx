f23ad8f780d7e7292e86f39b9bf493b324edc0f6
Kalidas Rajeev <kalidas.rajeev@friska.ai>
2026-02-11 22:14:02 +0530

    feat: integrate MUI components and update Card layout
    - Added MUI dependencies for Material UI and Emotion styling.
    - Refactored Card component to use MUI's Card, CardContent, CardMedia, and Button components for improved UI.
    - Removed unused H1 component and associated props in App.
    - Added a new image asset for the Card component.
    - Introduced basic CSS reset in site.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // props
 import Card from "./components/Card";
 import { Cart } from "./components/Cart";
-import H1 from "./components/H1";
 import { useState } from "react";
 // react is unidirectional
 // data -> parent to child
@@ -12,8 +11,6 @@ import { useState } from "react";
 // reuse it multiple times with different data
 
 const App = () => {
-  const name = "John";
-  const age = 30;
   const [selectedItems, setSelectedItems] = useState([]);
   const services = [
     {
@@ -40,8 +37,7 @@ const App = () => {
   };
 
   return (
-    <div>
-      <H1 nameValue={name} age={age} />
+    <div style={{ margin: 10 }}>
       {services.map((service) => (
         <Card
           key={service.id}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,64 @@
-const Card = (props) => {
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  CardActions,
+  CardMedia,
+} from "@mui/material";
+import Image1 from "../assets/web.jpg";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+
+const CardComponent = (props) => {
   const { service, addToCart, selectedItems, removeFromCart } = props;
   const isSelected = selectedItems.some((item) => item.id === service.id);
   return (
-    <div
-      style={{
-        border: "1px solid #ccc",
-        marginTop: 16,
-        padding: 16,
-        borderRadius: 8,
-      }}
-    >
-      <h2>{service.title}</h2>
-      <p>{service.price}</p>
-      {isSelected ? (
-        <button onClick={() => removeFromCart(service)}>Remove</button>
-      ) : (
-        <button onClick={() => addToCart(service)}>Buy Now</button>
-      )}
-    </div>
+    <Card sx={{ maxWidth: 345, marginBottom: 2 }}>
+      <CardMedia sx={{ height: 140 }} image={Image1} title="green iguana" />
+      <CardContent>
+        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
+          Course: {service.title}
+        </Typography>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <CurrencyRupeeIcon /> {service.price}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {isSelected ? (
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => removeFromCart(service)}
+            sx={{
+              boxShadow: "none",
+              backgroundColor: "error.main",
+            }}
+          >
+            Remove
+          </Button>
+        ) : (
+          <Button
+            size="small"
+            variant="contained"
+            onClick={() => addToCart(service)}
+            bgColor="primary.main"
+            sx={{
+              boxShadow: "none",
+            }}
+          >
+            Buy Now
+          </Button>
+        )}
+      </CardActions>
+    </Card>
   );
 };
 
-export default Card;
+export default CardComponent;
